b65210a8fdf28824875b81fa14b7a5ec729ba49a
Stepan Bogdanov <bogstepan@gmail.com>
2023-10-17 16:04:03 +0300

    2. Регистрация пользователя [#296069 #452937]

--- src/main/java/ru/job4j/accidents/controller/RegController.java
+++ src/main/java/ru/job4j/accidents/controller/RegController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ru.job4j.accidents.model.User;
 import ru.job4j.accidents.repository.data.AuthorityRepository;
 import ru.job4j.accidents.repository.data.UserRepository;
@@ -19,11 +20,16 @@ public class RegController {
     private final AuthorityRepository authorities;
 
     @PostMapping("/reg")
-    public String regSave(@ModelAttribute User user) {
+    public String regSave(@ModelAttribute User user, RedirectAttributes redirectAttributes) {
         user.setEnabled(true);
         user.setPassword(encoder.encode(user.getPassword()));
         user.setAuthority(authorities.findByAuthority("ROLE_USER"));
-        users.save(user);
+        try {
+            users.save(user);
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("userError", true);
+            return "redirect:/reg";
+        }
         return "redirect:/login";
     }
 
